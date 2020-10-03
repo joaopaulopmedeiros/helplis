3e4daa5cad13f1716077cfe09387e3a6e460d8dd
joao paulo <joaopaulopmedeiros@gmail.com>
2020-10-03 15:38:18 -0300

    front-end scaffolding

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,18 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import Routes from "./routes"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Helplis</code> getting started!
-        </p>
-      </header>
-    </div>
-  );
+    <Routes />
+  )
 }
 
-export default App;
+export default App
