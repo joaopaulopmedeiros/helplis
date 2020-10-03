faa18a5437cf47aa3cff2e4ec9553e04f73935e8
joao paulo <joaopaulopmedeiros@gmail.com>
2020-10-03 17:22:58 -0300

    login screen

--- web-client/src/pages/Login/index.js
+++ web-client/src/pages/Login/index.js
@@ -1,9 +1,50 @@
-import React from "react"
+import React from "react";
+
+import "./styles.css";
+
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-    return (
-        <h1>Login</h1>
-    )
-}
+  return (
+    <>
+      <div className="fluid-container">
+        <div className="container">
+          <div className="login-wrapper">
+            <div className="presentation-wrapper">
+              <img src={logo} alt="logo" className="logo-presentation" />
+              <h1>Faça seu login no DevHelper!</h1>
+            </div>
+            <div className="form-wrapper">
+              <form action="javascript:void(0)">
+                <div className="form-group">
+                  <input
+                    name="email"
+                    type="text"
+                    id="email"
+                    aria-label="email"
+                    placeholder="Username"
+                  />
+                  <input
+                    name="password"
+                    type="password"
+                    id="password"
+                    aria-label="password"
+                    placeholder="Password"
+                  />
+                  <div class="register-link-wrapper">
+                    <a href="javascript:void(0)">Ainda não possui uma conta?</a>
+                  </div>
+                  <button type="submit" class="submit-btn">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
